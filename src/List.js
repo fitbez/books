b708e1bbe97d2d3209615e0e6625f19b180f8d3c
Fitsum <fitbez@gmail.com>
2021-11-03 21:40:34 -0400

    setting up state and created components

---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function List({ books }) {
+  return (
+    <div>
+      {books.map((book) => {
+        const { id, author, title, image } = book;
+        return (
+          <article key={id} className="book">
+            <img src={image} alt={title} />
+            <div>
+              <h4>Author: {author}</h4>
+              <p>Book Title: {title}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+}
+
+export default List;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: 1,
+    author: "author one",
+    title: "book title one",
+    image: "image url one",
+  },
+  {
+    id: 2,
+    author: "book two",
+    title: "book title two",
+    image: "image url two",
+  },
+  {
+    id: 3,
+    author: "book three",
+    title: "book title three",
+    image: "image url three",
+  },
+];
+
+export default data;
